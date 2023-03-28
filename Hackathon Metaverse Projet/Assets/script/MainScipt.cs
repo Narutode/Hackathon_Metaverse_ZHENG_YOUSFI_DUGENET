f5a2892c2d6fe5ff78b3e2f0d886ed85a834efb5
Denis ZHENG <dzheng2@myges.fr>
2023-03-28 11:35:39 +0200

    add script

--- Hackathon Metaverse Projet/Assets/script/MainScipt.cs
+++ Hackathon Metaverse Projet/Assets/script/MainScipt.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MainScipt : MonoBehaviour
 {
+    public TextMeshProUGUI TextTemperature;
+    private int temperature;
+    private Color32 redColor = new Color32(255, 0, 0, 255); // couleur rouge
+    Color32 orangeColor = new Color32(255, 165, 0, 255);
     // Start is called before the first frame update
     void Start()
     {
-        
+        temperature = 15;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (temperature > 40)
+        {
+            TextTemperature.color = orangeColor; // changer la couleur du texte à orange
+        }
+        if (temperature > 60)
+        {
+            TextTemperature.color = redColor; // changer la couleur du texte à rouge
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        temperature++;
+        TextTemperature.text = temperature.ToString(); 
     }
 }
